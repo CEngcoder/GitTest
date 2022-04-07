a7bc4a2cc53c39ef2267db7d7a322b02d25034cf
CEngcoder <leizqk22810@126.com>
2022-04-07 22:02:46 +0800

    增加按钮

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include<QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPushButton* btn = new QPushButton(this);
+    btn->setFixedSize(20,20);
+
 }
 
 MainWindow::~MainWindow()
