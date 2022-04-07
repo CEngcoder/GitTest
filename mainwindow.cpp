e42916098148899e64fd0e93bb98ee4f7ca3d0b7
CEngcoder <leizqk22810@126.com>
2022-04-07 22:47:51 +0800

    增加标签

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include<QPushButton>
+#include<QLabel>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -9,6 +10,11 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     QPushButton* btn = new QPushButton(this);
     btn->setFixedSize(20,20);
+
+    QLabel* label = new QLabel(this);
+    label->setFixedSize(100,100);
+    label->setText("增加按钮");
+
 
 }
 
